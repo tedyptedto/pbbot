2c2049ae74836424fa1e244da7693efb8901ca73
tedyptedto <nomp5923@gmail.com>
2023-11-25 22:54:52 +0100

    add newuser

--- pbbot.py
+++ pbbot.py
@@ -37,6 +37,7 @@ copytraders = [
     {'discordUser': 'mani', 'bbUser': 'manicptrndr', 'bbCode': "ciOb3vGv0dp8JKJp4WTmeg%3D%3D"},
     {'discordUser': 'tedyptedto', 'bbUser': 'tedyptedtoCpTr', 'bbCode': "VAfEwFPZdNdfYGWiwy7V0g%3D%3D"},
     {'discordUser': 'tedyptedto', 'bbUser': 'tedySub2', 'bbCode': "W86y5Bo8c78Cy803QgMwMg%3D%3D"},
+    {'discordUser': 'tedyptedto', 'bbUser': 'Tedy57123TheBestOne', 'bbCode': "K%2Bupto5fn8zpUpIY0GvI%2FA%3D%3D"},
 ]
 
 stats_file = base_dir + "/config/stats.json"
